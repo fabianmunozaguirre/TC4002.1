aa967db8a1716801f44859b8de5b43f512d801f1
fabianmunozaguirre <79460621+fabianmunozaguirre@users.noreply.github.com>
2021-05-19 23:23:16 -0500

    Update Unit_Testing.py

--- Unit_Testing.py
+++ Unit_Testing.py
@@ -9,7 +9,7 @@ import math
 
 class TestStringMethods(unittest.TestCase): 
     def test_math_ceil(self):
-        result = math.ceil(1.4)
+        result = math.ceil(1.2)
         self.assertEqual(result, 2.0)
 
 if __name__ == '__main__':
